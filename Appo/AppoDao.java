acbed7e4d7764564d7ef162cf2ce21120fbdb645
tanvi0904 <tanvi.rajesh09@gmail.com>
2023-04-20 00:19:14 +0530

    appointment update

--- Appo/AppoDao.java
+++ Appo/AppoDao.java
@@ -10,7 +10,7 @@ public class AppoDao {
     private Connection connection;
 
     public AppoDao() throws SQLException {
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -22,18 +22,31 @@ public class AppoDao {
         connection = DriverManager.getConnection(url, username, password);
     }
 
-    public void addAppo(Report appo) throws SQLException {
-        String sql = "INSERT INTO appointment (pid, did, date_time,report) VALUES (?, ?, ?, ?)";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1, appo.getPid());
-        statement.setString(2, appo.getDid());
-        statement.setString(3, appo.getDate_time());
-        statement.setString(4, "Not yet");
-        statement.executeUpdate();
+    public void addAppo(Appo appo) throws SQLException {
+        // check if an appointment already exists with the same did and date_time
+        String checkSql = "SELECT pid FROM appointment WHERE did = ? AND date_time = ?";
+        PreparedStatement checkStatement = connection.prepareStatement(checkSql);
+        checkStatement.setString(1, appo.getDid());
+        checkStatement.setString(2, appo.getDate_time());
+        ResultSet resultSet = checkStatement.executeQuery();
+
+        // if an appointment already exists, throw an exception
+        if (resultSet.next()) {
+            throw new SQLException("An appointment with the same did and date_time already exists");
+        }
+
+        // insert the new appointment
+        String insertSql = "INSERT INTO appointment (pid, did, date_time, report) VALUES (?, ?, ?, ?)";
+        PreparedStatement insertStatement = connection.prepareStatement(insertSql);
+        insertStatement.setString(1, appo.getPid());
+        insertStatement.setString(2, appo.getDid());
+        insertStatement.setString(3, appo.getDate_time());
+        insertStatement.setString(4, "Not yet");
+        insertStatement.executeUpdate();
     }
 
-    public List<Report> getAllAppos() throws SQLException {
-        List<Report> appos = new ArrayList<>();
+    public List<Appo> getAllAppos() throws SQLException {
+        List<Appo> appos = new ArrayList<>();
         String sql = "SELECT * FROM appointment";
         PreparedStatement statement = connection.prepareStatement(sql);
         ResultSet resultSet = statement.executeQuery();
@@ -42,7 +55,7 @@ public class AppoDao {
             String did = resultSet.getString("did");
             String date_time = resultSet.getString("date_time");
             String report = resultSet.getString("report");
-            Report appo = new Report(pid, did, date_time, report);
+            Appo appo = new Appo(pid, did, date_time, report);
             appos.add(appo);
         }
         System.out.println(appos);
